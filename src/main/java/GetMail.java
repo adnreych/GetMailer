8b6e19e7f8f81ba69729613861af79dc51be4aa3
adnreych <47110538+adnreych@users.noreply.github.com>
2019-06-04 21:32:57 +0300

    handling situation if site not found

--- src/main/java/GetMail.java
+++ src/main/java/GetMail.java
@@ -18,16 +18,22 @@ public class GetMail {
 
 	Pattern pattern = Pattern.compile("[\\w-]+@[\\w-]+\\.[a-z]+", Pattern.MULTILINE | Pattern.CASE_INSENSITIVE);
 
-	public ArrayList<String> siteDomains = new ArrayList<String>();
+	private ArrayList<String> siteDomains = new ArrayList<String>();
+	private ArrayList<String> notFound = new ArrayList<String>();
 
 	void fillSiteDomains() {
 		for (StringBuilder q : query) {
 			String s = gj.getUrl(q);
-			siteDomains.add(s.substring(1, s.length() - 1)); // delete "" in JSON request
+			if (!s.equals(""))
+				siteDomains.add(s.substring(1, s.length() - 1)); // delete "" in JSON request
+			else
+				notFound.add("По запросу " + q + " сайт не найден");
+
 		}
 	}
 
 	private LinkedHashSet<String> emails = new LinkedHashSet<>();
+	private ArrayList<String> orgsEmails = new ArrayList<>();
 
 	void getMail() {
 		for (String domain : siteDomains) {
@@ -40,9 +46,27 @@ public class GetMail {
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
+
 			}
+			orgsEmails.add(domain + " " + emails);
+			emails.clear();
 		}
-		System.out.println(emails);
+		System.out.println(orgsEmails);
+		System.out.println(notFound);
+	}
+
+	public static void main(String[] args) {
+		ArrayList<StringBuilder> input = new ArrayList<StringBuilder>();
+		input.add(new StringBuilder("rush agency"));
+		input.add(new StringBuilder("irobot-crimea"));
+		input.add(new StringBuilder("экмус"));
+		input.add(new StringBuilder("экоумвельт"));
+		input.add(new StringBuilder("течпорт"));
+
+		GetMail gm = new GetMail();
+		gm.setQuery(input);
+		gm.fillSiteDomains();
+		gm.getMail();
 	}
 
 }
