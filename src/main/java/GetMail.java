eec078f87eae6394d814a54dcd15c42f48e18988
adnreych <47110538+adnreych@users.noreply.github.com>
2019-06-10 18:15:35 +0300

    added search emails in inner pages

--- src/main/java/GetMail.java
+++ src/main/java/GetMail.java
@@ -4,69 +4,66 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GetMail {
+	private GetJson gj = new GetJson();
+	private Pattern pattern = Pattern.compile("[\\w-]+@[\\w-]+\\.[a-z]+", Pattern.MULTILINE | Pattern.CASE_INSENSITIVE);
+	private Pattern innerPattern = Pattern.compile("[A-Za-z\\.]+(?=\">Контакты<)",
+			Pattern.MULTILINE | Pattern.CASE_INSENSITIVE);
 	private ArrayList<StringBuilder> query;
+	private ArrayList<String> siteDomains = new ArrayList<String>();
+	private ArrayList<String> notFound = new ArrayList<String>();
+	private LinkedHashSet<String> emails = new LinkedHashSet<>();
+	private ArrayList<String> orgsEmails = new ArrayList<>();
 
-	public ArrayList<StringBuilder> getQuery() {
-		return query;
-	}
-
-	public void setQuery(ArrayList<StringBuilder> query) {
+	GetMail(ArrayList<StringBuilder> query) {
 		this.query = query;
 	}
 
-	GetJson gj = new GetJson();
-
-	Pattern pattern = Pattern.compile("[\\w-]+@[\\w-]+\\.[a-z]+", Pattern.MULTILINE | Pattern.CASE_INSENSITIVE);
-
-	private ArrayList<String> siteDomains = new ArrayList<String>();
-	private ArrayList<String> notFound = new ArrayList<String>();
-
-	void fillSiteDomains() {
+	private void fillSiteDomains() {
 		for (StringBuilder q : query) {
 			String s = gj.getUrl(q);
 			if (!s.equals(""))
 				siteDomains.add(s.substring(1, s.length() - 1)); // delete "" in JSON request
 			else
 				notFound.add("По запросу " + q + " сайт не найден");
-
 		}
 	}
 
-	private LinkedHashSet<String> emails = new LinkedHashSet<>();
-	private ArrayList<String> orgsEmails = new ArrayList<>();
+	private String getInnerPage(StringBuilder domain) {
+		String inner = "Страница Контакты не найдена";
+		SourceHtml siteSource = new SourceHtml(new StringBuilder(domain));
+		StringBuilder source = siteSource.getSourceHtml();
+		Matcher innerMatcher = innerPattern.matcher(source.toString());
+		while (innerMatcher.find())
+			inner = source.substring(innerMatcher.start(), innerMatcher.end());
+		return inner;
+	}
+
+	private void searchMails(String domain) {
+		SourceHtml siteSource = new SourceHtml(new StringBuilder(domain));
+		StringBuilder source = siteSource.getSourceHtml();
+		Matcher matcher = pattern.matcher(source);
+		while (matcher.find()) {
+			try {
+				emails.add(source.substring(matcher.start(), matcher.end()));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		orgsEmails.add(domain + " " + emails + "\n");
+	}
 
 	void getMail() {
+		fillSiteDomains();
 		for (String domain : siteDomains) {
-			SourceHtml siteSource = new SourceHtml(new StringBuilder(domain));
-			StringBuilder source = siteSource.getSourceHtml();
-			Matcher matcher = pattern.matcher(source);
-			while (matcher.find()) {
-				try {
-					emails.add(source.substring(matcher.start(), matcher.end()));
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-
+			searchMails(domain);
+			if (emails.isEmpty()) {
+				String innerPage = domain + getInnerPage(new StringBuilder(domain));
+				searchMails(innerPage);
 			}
-			orgsEmails.add(domain + " " + emails);
 			emails.clear();
 		}
 		System.out.println(orgsEmails);
 		System.out.println(notFound);
-	}
-
-	public static void main(String[] args) {
-		ArrayList<StringBuilder> input = new ArrayList<StringBuilder>();
-		input.add(new StringBuilder("rush agency"));
-		input.add(new StringBuilder("irobot-crimea"));
-		input.add(new StringBuilder("экмус"));
-		input.add(new StringBuilder("экоумвельт"));
-		input.add(new StringBuilder("течпорт"));
-
-		GetMail gm = new GetMail();
-		gm.setQuery(input);
-		gm.fillSiteDomains();
-		gm.getMail();
 	}
 
 }
